bddc57b21fe7f36e7b55c50306c9eccc6b58a4a0
Rabi Mandal <100466028+rabimandal@users.noreply.github.com>
2022-07-26 23:05:50 +0530

    Add files via upload

---
+++ imagebazar/imagebazar/views.py
@@ -0,0 +1,48 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from myapp.models import  *
+
+
+
+def show_about_page(request):
+    print("about page request")
+    name = 'Rabi Kumar Mandal'
+    link = 'https://www.linkedin.com/in/rabimandal27/'
+
+    data = {
+        'name': name,
+        'link': link
+
+    }
+
+    return render(request, "about.html", data)
+
+
+def show_home_page(request):
+
+    cats = Category.objects.all()
+    images = Image.objects.all()
+
+    data = {'images': images, 'cats': cats}
+
+
+    return render(request, "home.html", data)
+
+
+def show_category_page(request, cid):
+    print(cid)
+    cats = Category.objects.all()
+
+
+    category = Category.objects.get(pk=cid)
+
+
+    images = Image.objects.filter(cat=category)
+    data = {'images': images, 'cats': cats}
+
+
+    return render(request, "home.html", data)
+
+
+def home(request):
+    return redirect('/home')
